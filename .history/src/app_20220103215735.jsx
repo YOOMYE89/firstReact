bcd5725a03eb65b86912d082c3d2fc8491c06848
YOOMYE89 <59650687+YOOMYE89@users.noreply.github.com>
2022-02-20 16:12:19 +0900

    Delete .history directory

--- .history/src/app_20220103215735.jsx
+++
@@ -1,9 +0,0 @@
-
-import './app.css';
-
-function App() {
-  return React.createElemnt('h1', {}, 'Hello:)~~')
-  // return <h1>Hello!! :)</h1>
-}
-
-export default App;
--- .history/src/app_20220103215855.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './app.css';
-
-function App() {
-  // return React.createElement('h1', {}, 'Hello:)~~') 바벨되면 위처럼 보임
-  return <h1>Hello!! :)</h1>
-}
-
-export default App;
